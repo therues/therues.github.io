8a37ab3cadb1eeae9e1c216418b5ffc98eb11723
Frank Rue <frue@bocodigital.com>
2015-04-10 02:09:43 -0400

    added portfolio items

--- js/app/app.js
+++ js/app/app.js
@@ -10,6 +10,21 @@ require([
 
   'use strict';
 
-  
+  $(function() {
+
+    $('body').scrollspy({
+      offset: parseInt( $('body').css("padding-top") )
+    });
+
+    $('.navbar-nav a').click(function(e) {
+
+      e.preventDefault();
+      if ($(this).attr("href")) {
+        TweenMax.to('body',1,{ scrollTop: $( $(this).attr("href") ).position().top - parseInt( $('body').css("padding-top") ) + 1, ease:Expo.easeOut });
+      }
+
+    });
+
+  });
 
 });
